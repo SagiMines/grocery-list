9e1e571a4988f845edf1eb5a09db7a48e39a8eb9
Sagi Mines <sagi1236@gmail.com>
2022-08-14 11:57:10 +0300

    Added the order summary component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import List from './List';
+import OrderSummary from './OrderSummary';
 import { groceries } from './DAL/data.js';
 import { useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
@@ -6,14 +7,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState({ groceries, shopping: [] });
+  const [data, setData] = useState({
+    groceries,
+    shopping: [],
+    summary: {
+      totalItems: 0,
+      totalPrice: 0,
+    },
+  });
 
   const handleGroceryClick = e => {
     const currentValue = e.target.getAttribute('value');
     data.shopping.push(currentValue);
+    data.summary.totalItems += 1;
     for (const grocery of data.groceries) {
       if (grocery.name === currentValue) {
         grocery.quantity -= 1;
+        data.summary.totalPrice += grocery.price;
         break;
       }
     }
@@ -33,6 +43,7 @@ function App() {
         <Col>
           <List data={data.shopping} title="Shopping" />
         </Col>
+        <OrderSummary data={data.summary} />
       </Row>
     </div>
   );
